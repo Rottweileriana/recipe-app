65dc862caefd9550f7d2b2f4fb719812ee662267
Rebecka Åberg <rebecka.aberg@student.kyh.se>
2022-08-26 18:08:51 +0200

    update

---
+++ src/logic/TipsList.js
@@ -0,0 +1,9 @@
+const tipsList = [
+
+    {
+        "name": "Potatismos över",
+        "origin": "Sparas kokbok",
+        "page": 12,
+        "description": ["Potatismos som blir över behöver aldrig förbli oanvän. Moset kan kombineras med andra rester på nästa hur många sätt som helst. Blanda ev först moset med 1 uppvispat ägg och lite vetemjöl. Eller använd moset direk som det är. Gör sen tillexempel så här:", "Läggmoset i botten på en smord ugnssäker form. Häll överbliven köfärssås over, eller överbliven fisk med sås. Eller smula över stekt becon eller isterband.", "Eller lägg överblivna kokta grönsaker i en form och häll över en vit grundsås, eller kanske svampstuvning som blev kvar. Klick potatismos runt om.", "Strö ev lite ost över restyrätten och värm i ungnen, 225°C, i 15-20 min.", "Ett gott fräs till potatismos får man genom att fräsa strimlor av becon, gullök, morot, och en bit rotselleri. Lägg först beconet i pannan och efter en liten stund grönsakerna. Fräs till de är mjuka, ca 5 minuter.", "Eller fräs först beconstrimlor och en hackad gul lök och tillsätt sedan böngrodar från burk. Låt alltsammans fräsa några minuer. Rör om då och då. Smaksätt med peppar."]
+    }
+]
